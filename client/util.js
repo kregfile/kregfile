032727591049a05c614b48f765ef8b299d416013
RealDolos <dolos@cock.li>
2018-04-15 10:10:23 +0200

    Satan's little helpers

--- client/util.js
+++ client/util.js
@@ -1,5 +1,20 @@
 "use strict";
 
+export function dom(type, options) {
+  const {attrs = {}, text = "", classes = []} = options || {};
+  const el = document.createElement(type);
+  for (const [an, av] of Object.entries(attrs)) {
+    el.setAttribute(an, av);
+  }
+  if (Array.isArray(classes) && classes.length) {
+    el.classList.add(...classes);
+  }
+  if (text) {
+    el.textContent = text;
+  }
+  return el;
+}
+
 export function nukeEvent(e) {
   e.preventDefault();
   e.stopPropagation();
