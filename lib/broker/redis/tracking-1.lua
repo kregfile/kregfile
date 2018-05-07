477f56f30e12e4cbda409c8382d8d761ec4c81da
RealDolos <dolos@cock.li>
2018-05-07 13:06:52 +0200

    Simplify tracking code

--- lib/broker/redis/tracking-1.lua
+++ lib/broker/redis/tracking-1.lua
@@ -1,4 +1,4 @@
-local EXPIRE = 60
+local EXPIRE = 20
 
 local key = KEYS[1]
 local op = ARGV[1]
@@ -14,23 +14,20 @@ local setskey = key .. ":sets"
 
 local expire = function(force)
   local res = {}
-  local expkey = key .. ":exp"
   if force == 0 then
-    local ct = redis.call("TTL", expkey)
+    local ct = redis.call("TTL", key)
     if ct > 0 then
       return res
     end
   end
-  local ct = redis.call("SET", expkey, "0", "EX", EXPIRE)
-  local exp = 0
   redis.call("DEL", key)
   for i,k in ipairs(redis.call("SMEMBERS", setskey)) do
-    if redis.call("TTL", tokey(k)) <= 0 then
+    local pkey = tokey(k)
+    if redis.call("TTL", pkey) <= 0 then
       redis.call("SREM", setskey, k)
-      redis.call("DEL", tokey(k))
-      exp = 1
+      redis.call("DEL", pkey)
     else
-      local arr = redis.call("ZRANGE", tokey(k), 0, -1, "WITHSCORES")
+      local arr = redis.call("ZRANGE", pkey, 0, -1, "WITHSCORES")
       for i = 1, table.getn(arr), 2 do
         local itemkey = arr[i]
         local itemvalue = tonumber(arr[i + 1])
@@ -44,13 +41,14 @@ local expire = function(force)
       end
     end
   end
+  redis.call("EXPIRE", key, EXPIRE)
   local rv = {}
   for k, v in pairs(res) do
-    table.insert(rv, {k, v})
+    if v > 0 then
+      table.insert(rv, {k, v})
+    end
   end
-  if exp == 1 then
-    redis.call("PUBLISH", key, cjson.encode({{op="exp", v=rv}}))
-  end
+  redis.call("PUBLISH", key, cjson.encode({{op="exp", v=rv}}))
   return rv
 end
 
@@ -69,25 +67,24 @@ if op == "refresh" then
   return
 
 elseif op == "incr" then
-  refresh()
   expire(0)
   item = ARGV[3]
   redis.call("ZINCRBY", pidkey, 1, item)
+  refresh()
   cvalue = tonumber(redis.call("ZINCRBY", key, 1, item))
   redis.call("PUBLISH", key, cjson.encode({{op="s", k=item, v=cvalue}}))
   return cvalue
 
 elseif op == "decr" then
-  refresh()
   expire(0)
   item = ARGV[3]
   redis.call("ZINCRBY", pidkey, -1, item)
+  refresh()
   cvalue = tonumber(redis.call("ZINCRBY", key, -1, item))
   redis.call("PUBLISH", key, cjson.encode({{op="s", k=item, v=cvalue}}))
   return cvalue
 
 elseif op == "del" then
-  refresh()
   expire(0)
   item = ARGV[3]
   for i, k in ipairs(redis.call("SMEMBERS", setskey)) do
