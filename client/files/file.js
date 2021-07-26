11b7b13f3cc09a16b8be1f76f78ececd4b664037
voron <voron@voron-desktop>
2021-07-26 10:47:01 +0300

    attr -> attrs

--- client/files/file.js
+++ client/files/file.js
@@ -86,7 +86,7 @@ export default class File extends BaseFile {
 
     this.sizeEl = dom("span", {
       classes: ["size"],
-      attr: {"title" : file.size }, /* Added line to tooltip the actual size */
+      attrs: { "title" : file.size }, /* Added line to tooltip the actual size */
       text: toPrettySize(file.size)
     });
     this.detailEl.appendChild(this.sizeEl);
